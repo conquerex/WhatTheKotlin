9736d4d6381cd18964ec7abd89ac9a96e88b91fb
Jongkook <conquerex@gmail.com>
2020-10-30 19:50:15 +0900

    chpater5. 클래스 확장하기

--- src/main/kotlin/Chapter5.kt
+++ src/main/kotlin/Chapter5.kt
@@ -16,6 +16,60 @@ fun main(args: Array<String>) {
 
     var interfaceimpl = InterfaceImpl(object : A {})
     interfaceimpl.someMethod()
+
+    /**
+     * 코틀린에서 다중 오버라이드된 메소드로 복잡한 인터페이스를 구현하는 방법
+     */
+    Simple().callMethod()
+
+    /**
+     * 클래스 확장하기(상속 및 확장 함수)
+     */
+    BBB("hi").foo()
+
+}
+
+class DDD(str: String) : CCC() {
+    override fun methodCCC() {
+        TODO("Not yet implemented")
+    }
+
+}
+
+abstract class CCC {
+    abstract fun methodCCC()
+    fun imple() {}
+}
+
+class BBB(text: String) : AAA(text) {
+    override fun foo() {
+        println("foooo from BBB")
+    }
+}
+
+open class AAA(str: String) {
+    open fun foo() {
+        println("fooooo")
+    }
+
+    final fun bar() {
+        println("barrrrrrr")
+    }
+}
+
+class BB(val randomString: String) : AA(randomString)
+
+class BB2 : AA {
+    constructor(ran: String) : super(ran)
+    constructor(ran: String, ranInt: Int) : super(ran)
+}
+
+open class AA(val str: String)
+
+class Simple : A {
+    fun callMethod() {
+        bar()
+    }
 }
 
 class InterfaceImpl2(val a: A) : A by a {
