90972dfd7cb30cbe3f6631a3e75f59f8d258c88e
Jongkook <conquerex@gmail.com>
2020-11-06 17:48:55 +0900

    chapter7. InputReader를 이용해 파일의 모든 데이터 읽기

--- src/main/kotlin/Chapter7.kt
+++ src/main/kotlin/Chapter7.kt
@@ -25,4 +25,30 @@ fun main(args: Array<String>) {
     listOfLines.forEach {
         println("$ " + it)
     }
+
+    /**
+     * InputReader를 이용해 파일의 모든 데이터 읽기
+     */
+    val inputStream2: InputStream = File("/Users/jongkook/dev/WhatTheKotlin/Android11Blog.txt").inputStream()
+    val inputString2 = inputStream2.reader().use { it.readText() }
+    println(inputString2)
+
+    val inputString3 = File("/Users/jongkook/dev/WhatTheKotlin/Android11Blog.txt").reader().use {
+        it.readText()
+    }
+    println(inputString3)
+
+    /**
+     * InputReader를 이용해 파일로부터 한줄씩 읽기
+     */
+    val listOfLines2 = mutableListOf<String>()
+    val inputStream4: InputStream = File("/Users/jongkook/dev/WhatTheKotlin/Android11Blog.txt").inputStream()
+    inputStream4.reader().useLines { lines ->
+        lines.forEach {
+            listOfLines2.add(it)
+        }
+    }
+    listOfLines2.forEach {
+        println("** $it")
+    }
 }
